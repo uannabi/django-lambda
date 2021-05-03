0357f91b5ca3cf2381c23bfba3697f49b7e030b6
uannabi <zahid_sc@hotmail.fr>
2021-05-03 11:53:19 +0600

    added git ignore

--- django_aws_lambda/django_aws_lambda/settings.py
+++ django_aws_lambda/django_aws_lambda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nrt0#_)1%8&!i&n797as)zhckx&3x5tjbn4-yoh4thc*8b(7pt'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
