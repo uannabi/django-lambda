a1de7e5b51f20d55a43edc2fad848ba687b7a273
uannabi <zahid_sc@hotmail.fr>
2021-05-03 12:10:27 +0600

    manage dot py

--- django_aws_lambda/django_aws_lambda/urls.py
+++ django_aws_lambda/django_aws_lambda/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
+
+from hello.views import hello
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
+    path('<path:resource>', hello),
 ]
